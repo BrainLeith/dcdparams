8ef6f36aed3b0d60c91445a93ba8ae57b6b539dc
Johnson <SchrodingersFish@outlook.com>
2023-06-23 18:30:04 +0800

    #修改 增加查询token计算功能

--- chatGPT.py
+++ chatGPT.py
@@ -1,11 +1,13 @@
 import datetime
 import requests
+import tiktoken
 from revChatGPT.V3 import Chatbot
 
 proxies = {
     'http': 'http://127.0.0.1:7890',
     'https': 'http://127.0.0.1:7890',
 }
+
 
 def get_key(apikey):
     subscription_url = "https://api.openai.com/v1/dashboard/billing/subscription"
@@ -48,10 +50,18 @@ def get_key(apikey):
            f"\n" + recent
 
 
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
 if __name__ == '__main__':
     # chatbot = Chatbot(api_key="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")
     # chatbot.proxy = proxies
     # for data in chatbot.ask_stream("你好，请帮我写一篇500字左右的关于电影《诺曼底登陆》的剧情介绍"):
     #     print(data, end="", flush=True)
     # 查询key的余额
-    print(get_key(apikey="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"))
+    # print(get_key(apikey="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"))
+    # 查询多少个token 2种编码方式 cl100k_base text-embedding-ada-002
+    print(num_tokens_from_string("你好，请问你几岁啊!", "cl100k_base"))
