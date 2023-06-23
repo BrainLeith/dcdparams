4b1281ae32985e6d364db92144122c0433b5f250
Johnson <SchrodingersFish@outlook.com>
2023-06-23 18:13:17 +0800

    #修改 增加查询key余额的功能

--- chatGPT.py
+++ chatGPT.py
@@ -1,12 +1,57 @@
+import datetime
+import requests
 from revChatGPT.V3 import Chatbot
 
 proxies = {
-  'http': 'http://127.0.0.1:7890',
-  'https': 'http://127.0.0.1:7890',
+    'http': 'http://127.0.0.1:7890',
+    'https': 'http://127.0.0.1:7890',
 }
 
+def get_key(apikey):
+    subscription_url = "https://api.openai.com/v1/dashboard/billing/subscription"
+    headers = {"Authorization": "Bearer " + apikey,
+               "Content-Type": "application/json"}
+    subscription_response = requests.get(subscription_url, headers=headers)
+    if subscription_response.status_code == 200:
+        data = subscription_response.json()
+        total = data.get("hard_limit_usd")
+    else:
+        return subscription_response.text
+    # start_date设置为今天日期前99天
+    start_date = (datetime.datetime.now() - datetime.timedelta(days=99)).strftime("%Y-%m-%d")
+    # end_date设置为今天日期+1
+    end_date = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    billing_url = f"https://api.openai.com/v1/dashboard/billing/usage?start_date={start_date}&end_date={end_date}"
+    billing_response = requests.get(billing_url, headers=headers)
+    if billing_response.status_code == 200:
+        data = billing_response.json()
+        total_usage = data.get("total_usage") / 100
+        daily_costs = data.get("daily_costs")
+        # 这个10就是指10天，可以自己调整～
+        days = min(30, len(daily_costs))
+        recent = f"##### 最近{days}天使用情况  \n"
+        for i in range(days):
+            cur = daily_costs[-i - 1]
+            date = datetime.datetime.fromtimestamp(cur.get("timestamp")).strftime("%Y-%m-%d")
+            line_items = cur.get("line_items")
+            cost = 0
+            for item in line_items:
+                cost += item.get("cost")
+            recent += f"\t{date}\t{(cost / 100):.2f} \n"
+    else:
+        return billing_response.text
+
+    return f"\n#### 监控key为：{apikey[:-25] + '*' * 25}\n" \
+           f"#### 总额:\t{total:.2f}  \n" \
+           f"#### 已用:\t{total_usage:.2f}  \n" \
+           f"#### 剩余:\t{total - total_usage:.2f}  \n" \
+           f"\n" + recent
+
+
 if __name__ == '__main__':
-    chatbot = Chatbot(api_key="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")
-    chatbot.proxy = proxies
-    for data in chatbot.ask_stream("你好，请帮我写一篇小学作文，题目叫：我的爸爸"):
-        print(data, end="", flush=True)
+    # chatbot = Chatbot(api_key="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")
+    # chatbot.proxy = proxies
+    # for data in chatbot.ask_stream("你好，请帮我写一篇500字左右的关于电影《诺曼底登陆》的剧情介绍"):
+    #     print(data, end="", flush=True)
+    # 查询key的余额
+    print(get_key(apikey="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"))
